403e85ae787501d36a5d28957521c5b18d228f47
Kyryl <kieremieiev@outlook.com>
2023-04-25 01:01:44 +0200

    clean up code, make html from js, remove all html that is made in js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,69 @@
 class Header{
+    CreateHtml(){
+        this.headerLogo = document.createElement("i");
+        this.headerLogo.classList = "fa-regular fa-face-smile";
 
+        this.headerFigure = document.createElement("figure");
+        this.headerFigure.classList = "header__logo";
+
+        this.headerText = document.createElement("h1");
+        this.headerText.classList = "header__text";
+        this.headerText.innerText = "Collection of Happiness";
+
+        this.header = document.createElement("header");
+        this.header.classList = "header";
+        
+        //figure append i
+        this.headerFigure.appendChild(this.headerLogo);
+        //header append stuff
+        this.header.appendChild(this.headerFigure);
+        this.header.appendChild(this.headerText);
+        document.getElementsByTagName("body")[0].appendChild(this.header);
+    }
 }
 
 class Footer{
+    CreateHtml(){
+        this.footerParagraph = document.createElement("p");
+        this.footerParagraph.innerText = "Gemaakt door - Kyryl Ieremieiev SD2D MediaCollege";
 
+        this.footer = document.createElement("footer");
+        this.footer.appendChild(this.footerParagraph);
+
+        document.getElementsByTagName("body")[0].appendChild(this.footer);
+    }
 }
 
 class LeftPanel{
     constructor(){
         this.episode = document.getElementsByClassName("episodes__episode")
+    }
+
+    CreateHtml(){
+        this.episodesUl = document.createElement("ul");
+        this.episodesUl.classList = "episodes";
+        for(let i = 0; i < 4; i++){
+            this.episodeDate = document.createElement("p");
+            this.episodeDate.classList = "episodes__date";
+            this.episodeImg = document.createElement("img");
+            this.episodeImg.classList = "episodes__image";
+            this.episodeImg.setAttribute("alt", "episode cover image");
+            this.episodeTitle = document.createElement("h2");
+            this.episodeTitle.classList = "episodes__title";
+
+            this.episodeLi = document.createElement("li");
+            this.episodeLi.classList = "episodes__episode";
+            this.episodeLi.appendChild(this.episodeDate);
+            this.episodeLi.appendChild(this.episodeImg);
+            this.episodeLi.appendChild(this.episodeTitle);
+
+            this.episodesUl.appendChild(this.episodeLi);
+        }
+
+        this.episodeSection = document.createElement("section");
+        this.episodeSection.classList = "leftSection";
+        this.episodeSection.appendChild(this.episodesUl)
+        document.getElementsByTagName("main")[0].appendChild(this.episodeSection);
     }
 
     ApplyDataToEpisode(data){
@@ -72,14 +127,69 @@ class RightPanel{
         this.featured = document.getElementsByClassName("featuredEpisode");
     }
 
+    CreateHtml(){
+        this.featuredEpisodeDate = document.createElement("p");
+        this.featuredEpisodeDate.classList = "featuredEpisode__datum";
+
+        this.featuredEpisodeImage = document.createElement("img");
+        this.featuredEpisodeImage.classList = "featuredEpisode__image";
+        this.featuredEpisodeImage.setAttribute("alt", "cover of the featured episode");
+
+        this.featuredEpisodeTitle = document.createElement("h2");
+        this.featuredEpisodeTitle.classList = "featuredEpisode__title";
+        
+        this.featuredFigure = document.createElement("figure");
+        this.featuredFigure.classList = "featuredEpisode";
+        //figure append
+        this.featuredFigure.appendChild(this.featuredEpisodeDate);
+        this.featuredFigure.appendChild(this.featuredEpisodeImage);
+        this.featuredFigure.appendChild(this.featuredEpisodeTitle);
+
+
+        this.featuredEpisodeParagraph = document.createElement("p");
+        this.featuredEpisodeParagraph.classList = "featuredEpisode__paragraph";
+
+        this.featuredAudio = document.createElement("a");
+        this.featuredAudio.classList = "featuredEpisode__audio";
+        this.featuredAudio.setAttribute("id", "js--audio")
+        this.featuredAudio.innerText = "Audio afspelen";
+
+        this.featuredSource = document.createElement("a");
+        this.featuredSource.classList = "featuredEpisode__source";
+        this.featuredSource.setAttribute("id", "js--source")
+        this.featuredSource.innerText = "Source >";
+
+        this.navigation = document.createElement("nav");
+        this.navigation.classList = "featuredEpisode__navigation";
+
+        //nav append a
+        this.navigation.appendChild(this.featuredAudio);
+        this.navigation.appendChild(this.featuredSource);
+
+        this.rightSection = document.createElement("section");
+        this.rightSection.classList = "rightSection";
+
+        //rightSection Append
+        this.rightSection.appendChild(this.featuredFigure);
+        this.rightSection.appendChild(this.featuredEpisodeParagraph);
+        this.rightSection.appendChild(this.navigation);
+
+        this.main = document.createElement("main");
+
+        this.main.appendChild(this.rightSection);
+        
+        document.getElementsByTagName("body")[0].appendChild(this.main);
+    }
+
     ApplyDataToFeaturedEpisode(data, currentFeatured){
         this.applyDataToRightPanel = new ApplyDataToRightPanel(data, this.featured);
         this.applyDataToRightPanel.ApplyDataToFeaturedDate(currentFeatured);
         this.applyDataToRightPanel.ApplyDataToFeaturedCover(currentFeatured);
         this.applyDataToRightPanel.ApplyDataToFeaturedTitle(currentFeatured);
         this.applyDataToRightPanel.ApplyDataToFeaturedText(currentFeatured);
+        this.applyDataToRightPanel.ApplyDataToFeaturedAudio(currentFeatured);
+        this.applyDataToRightPanel.ApplyDataToFeaturedSource(currentFeatured);
     }
-
 }
 
 class ApplyDataToRightPanel{
@@ -104,6 +214,16 @@ class ApplyDataToRightPanel{
         this.featuredText = document.getElementsByClassName("featuredEpisode__paragraph")[0];
         this.featuredText.innerText = Object.values(this.data.episodes[currentFeatured])[3]
     }
+
+    ApplyDataToFeaturedAudio(currentFeatured){
+        this.featuredAudio = document.getElementById("js--audio");
+        this.featuredAudio.setAttribute("href", Object.values(this.data.episodes[currentFeatured])[4]);
+    }
+
+    ApplyDataToFeaturedSource(currentFeatured){
+        this.featuredSource = document.getElementById("js--source");
+        this.featuredSource.setAttribute("href", Object.values(this.data.episodes[currentFeatured])[5]);
+    }
 }
 
 class DetailCard{
@@ -126,6 +246,15 @@ class App{
         this.getData = new GetData();
         this.leftPanel = new LeftPanel();
         this.rightPanel = new RightPanel();
+        this.header = new Header();
+        this.footer = new Footer();
+
+
+        this.header.CreateHtml();
+        this.rightPanel.CreateHtml();
+        this.leftPanel.CreateHtml();
+        this.footer.CreateHtml();
+
         this.getData.getData().then(()=>{
             this.data = this.getData.data;
             this.leftPanel.ApplyDataToEpisode(this.data);
